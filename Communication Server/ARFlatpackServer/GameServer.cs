28fbe9a6e417f044f0597725d3596ad8596f9de8
grant1212126 <grant1212126@yahoo.co.uk>
2024-03-02 22:00:20 +0000

    Started server creation

---
+++ Communication Server/ARFlatpackServer/GameServer.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Title = "AR Server";
+            Console.ReadKey();
+        }
+    }
+}
--- Communication Server/ARFlatpackServer/Server.cs
+++ Communication Server/ARFlatpackServer/Server.cs
@@ -1,6 +1,37 @@
-﻿namespace GameServer;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer;
 
 public class Server
 {
+    public static int MaxPlayers { get; private set;}
+    
+    public static int Port { get; private set; }
+
+    private static TcpListener tcpListener;
+
+    public void Start(int _maxPlayers, int _port)
+    {
+        MaxPlayers = _maxPlayers;
+        Port = _port;
+
+        Console.WriteLine("Starting Server...");
+        
+        tcpListener = new TcpListener(IPAddress.Any, Port);
+        tcpListener.Start();
+        tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+        
+        Console.WriteLine("Started on {Port}.");
+    }
+
+    private static void TCPConnectCallback(IAsyncResult _result)
+    {
+        TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
+        tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+    }
     
 }
